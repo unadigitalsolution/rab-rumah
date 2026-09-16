f7a59be73f699452ad9d0ab6a2de2227aa7ba7ba
unadigitalsolution <fathulwahab884@gmail.com>
2026-09-17 02:02:55 +0700

    test(flutter): use package import for RAB engine

--- flutter/test/rab_engine_test.dart
+++ flutter/test/rab_engine_test.dart
@@ -1,34 +1,17 @@
 import 'package:flutter_test/flutter_test.dart';
-import '../lib/rab_engine.dart';
+import 'package:rab_rumahku/rab_engine.dart';
 
 void main() {
   test('calculates geometry, materials, labor and total', () {
     final project = <String, dynamic>{
       'building': {
-        'length': 6,
-        'width': 6,
-        'floors': 1,
-        'wallHeight': 3,
-        'roofPitch': 30,
-        'overhang': .5,
-        'bathrooms': 1,
-        'terraceArea': 5,
-        'carportArea': 12,
-        'openings': {
-          'doors': 4,
-          'windows': 5,
-          'doorWidth': .9,
-          'doorHeight': 2.1,
-          'windowWidth': 1.2,
-          'windowHeight': 1.2,
-        },
+        'length': 6, 'width': 6, 'floors': 1, 'wallHeight': 3, 'roofPitch': 30, 'overhang': .5,
+        'bathrooms': 1, 'terraceArea': 5, 'carportArea': 12,
+        'openings': {'doors': 4, 'windows': 5, 'doorWidth': .9, 'doorHeight': 2.1, 'windowWidth': 1.2, 'windowHeight': 1.2},
       },
       'spec': {'wall': 'lightbrick', 'floor': 'tile', 'roofCover': 'genteng', 'ceiling': 'gypsum'},
       'waste': {'material': 5, 'tile': 7, 'brick': 5, 'paint': 5, 'roof': 5},
-      'materialWaste': <String, dynamic>{},
-      'coefficients': <String, dynamic>{},
-      'laborPriceOverrides': <String, dynamic>{},
-      'priceOverrides': <String, dynamic>{},
+      'materialWaste': <String, dynamic>{}, 'coefficients': <String, dynamic>{}, 'laborPriceOverrides': <String, dynamic>{}, 'priceOverrides': <String, dynamic>{},
       'extras': {'equipment': 5, 'overhead': 5, 'contingency': 5, 'transport': 0, 'profit': 0},
     };
     final result = RabEngine.calculate(project);
@@ -47,11 +30,8 @@ void main() {
     final project = <String, dynamic>{
       'building': {'length': 6, 'width': 6, 'floors': 1, 'wallHeight': 3, 'bathrooms': 1, 'openings': {'doors': 1, 'windows': 2}},
       'spec': {'wall': 'lightbrick', 'floor': 'tile', 'roofCover': 'genteng', 'ceiling': 'gypsum'},
-      'waste': {'material': 0, 'tile': 0, 'brick': 0, 'paint': 0, 'roof': 0},
-      'priceOverrides': {'cement': 100000},
-      'laborPriceOverrides': <String, dynamic>{},
-      'coefficients': <String, dynamic>{},
-      'materialWaste': <String, dynamic>{},
+      'waste': {'material': 0, 'tile': 0, 'brick': 0, 'paint': 0, 'roof': 0}, 'priceOverrides': {'cement': 100000},
+      'laborPriceOverrides': <String, dynamic>{}, 'coefficients': <String, dynamic>{}, 'materialWaste': <String, dynamic>{},
       'extras': {'equipment': 0, 'overhead': 0, 'contingency': 0, 'transport': 0, 'profit': 0},
     };
     final result = RabEngine.calculate(project);
