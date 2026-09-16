3b74bb621fab17f346cfcd7bb9f3fcc65da76c5b
unadigitalsolution <fathulwahab884@gmail.com>
2026-09-17 02:27:02 +0700

    test(flutter): align widget smoke test with current home UI

--- flutter/test/widget_test.dart
+++ flutter/test/widget_test.dart
@@ -4,8 +4,9 @@ import 'package:rab_rumahku/main.dart';
 void main() {
   testWidgets('RAB Rumahku app starts', (WidgetTester tester) async {
     await tester.pumpWidget(const RabApp());
+    await tester.pumpAndSettle();
 
-    expect(find.text('🏠 RAB Rumahku'), findsOneWidget);
+    expect(find.text('RAB Rumahku'), findsOneWidget);
     expect(find.text('BUAT RAB RUMAH'), findsOneWidget);
   });
 }
